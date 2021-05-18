9e29fe993d8ad77f428bc959aa3d0ec760b57042
jaosherlyr <70427806+jaosherlyr@users.noreply.github.com>
2021-05-18 16:14:37 +0800

    grammar correction

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,10 +20,10 @@ class About extends Component {
 				<div className="content">
 					<h1>I am <span className='gold'>Sherly R. Jao</span>, I will be a <span className="gold">Software Engineer</span>.</h1>
 					<p>
-						I was born in Minglanilla, Cebu and was raised in the City of Naga down south and living in Lapu-Lapu City
+						I was born in Minglanilla, Cebu and was raised in the City of Naga down south, now I am currently living in Lapu-Lapu City.
 					</p>
 					<p>
-						Since I was young, I wanted to follow my father and become a Software Engineer
+						Since I was young, I wanted to follow my father and become a Software Engineer.
 					</p>
 					<p>
 						As of now I have few artworks commissioned and I seek to incorporate my artistic skills to my future potential web development career.
