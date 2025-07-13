648e0bb5baddbc86a6c1046ae2134d50ad65a3ed
Sherly <mac@siMAC-13.local>
2025-07-13 16:24:43 +0800

    style: update logo and added secondary font

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
 import { IoSunny, IoMoon } from "react-icons/io5";
-
 import { useTheme } from "../context/ThemeContext";
 
 import styles from "./navbar.module.scss";
+import LogoLight from "../assets/Logo-light.svg";
+import LogoDark from "../assets/Logo-dark.svg";
 
 export default function Navbar() {
     const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={`${styles.navbar} ${theme === 'dark' ? styles.dark : ''}`}>
-            <div>
-                <h2>Logo</h2>
+            <div className={styles.logo}>
+                <img src={theme === 'light' ? LogoLight : LogoDark} alt="Logo" />
+                <p>JAO</p>
             </div>
 
             <div className={styles.tab}>
