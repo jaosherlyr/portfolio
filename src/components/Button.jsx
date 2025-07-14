e16f573c829394b2690a174c8772c41d748213d7
Sherly <mac@siMAC-13.local>
2025-07-14 17:20:58 +0800

    styled homedetails and adjusted background colors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import styles from "./Button.module.scss";
 
-export default function Button({ children, theme }) {
-
-    return (
-        <div className={`${styles.btnContainer} ${theme === 'light' ? 'light' : ''}`}>
-            {children}
-        </div>
-    );
+export default function Button({ children, onClick, theme }) {
+  return (
+    <div 
+        className={`${styles.btnContainer} ${theme === 'dark' ? styles.dark : ''}`}
+        onClick={onClick}    
+    >
+      {children}
+    </div>
+  );
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,26 +1,36 @@
 import "./HomePage.scss";
 import Button from "../components/Button.jsx";
-import { IoArrowForwardOutline } from "react-icons/io5";
 
-import { useTheme } from "../context/ThemeContext";
+import { IoArrowForwardOutline } from "react-icons/io5";
+import { useTheme } from "../context/ThemeContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const { theme } = useTheme();
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    navigate('/works');
+  }
 
   return (
     <div className="container">
       <div className="placeholder"></div>
+
       <div className="homedetails">
-        <span className="title">Hi! I am <span className="name">Sherly R. Jao</span></span>
-        <span className="subtitle">Artist | Web Developer & Designer</span>
+        <span className="title">Hi! I am <span className="name whitespace-nowrap">Sherly R. Jao</span></span>
+        <span className="subtitle ">Artist | Web Developer & Designer</span>
+
         <p>
-          I code and I draw. Based in the Philippines. <br />
+          I code and I draw, and I'm from the <strong>Philippines 🇵🇭</strong> <br/>
           Just trying my best to navigate life with the skills I have.
         </p>
-        <Button theme={theme}> 
+
+        <Button theme={theme} onClick={onButtonClick}>
           <IoArrowForwardOutline />
           Check out my works!
         </Button>
+
       </div>
     </div>
   );
